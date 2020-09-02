c2eb1650cdbba8c89a2ee43f4544134c82cfac87
1 <a1@1s-MacBook-Pro.lan>
2020-09-02 16:44:28 +0300

    chore(eslint): added file with TSlint rules, added necessary library

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,15 @@ module.exports = {
   rules: {
     'react/prop-types': 'off',
     'react/no-unescaped-entities': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     'no-console': 'warn',
+    
+    '@typescript-eslint/no-explicit-any': 'on',
+    '@typescript-eslint/no-empty-interface': 'on',
+    '@typescript-eslint/explicit-member-accessibility': 'explicit',
+    '@typescript-eslint/no-magic-numbers': 'on',
   },
 };
